7312285543bebce1e5624a4fb3c99df44079688b
Alexandre Oliveira <alexandre.yukon@gmail.com>
2024-04-30 11:40:33 -0400

    Adpater and creating contact lists

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.devspace.recyclerview
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val rvList = findViewById<RecyclerView>(R.id.rv_list)
     }
 }
+
+val contacts = listOf(
+    Contact(
+        name = "Bini",
+        phone = "+1 (647) 871-3073",
+        R.drawable.sample8
+    ),
+    Contact(
+        name = "Pamela Shein",
+        phone = "+1 (647) 871-6743",
+        R.drawable.sample11
+    ),
+    Contact(
+        name = "Sergie",
+        phone = "+1 (647) 871-0990",
+        R.drawable.sample14
+    ),
+    Contact(
+        name = "Jujuba",
+        phone = "+1 (395) 871-1242",
+        R.drawable.sample3
+    ),
+    Contact(
+        name = "Julio Miguel",
+        phone = "+1 (647) 871-8877",
+        R.drawable.sample12
+    )
+)
